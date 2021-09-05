c7f4d2652ae552060f2365146a58b25b041ff73c
syeol.kim <syeol.kim@daum.net>
2021-09-05 16:02:30 +0900

    [PS-2] feat: Use multi thread in batch
    - Set chunkSize to 100
    - Set poolSize to 4
    - Set batch-schedule to 4:10 pm for testing

--- src/main/java/org/localdevelopers/payfinderbatch/config/BatchConfig.java
+++ src/main/java/org/localdevelopers/payfinderbatch/config/BatchConfig.java
@@ -9,39 +9,48 @@ import org.localdevelopers.payfinderbatch.reader.StoreItemReader;
 import org.localdevelopers.payfinderbatch.writer.StoreItemWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.MongoItemWriter;
+import org.springframework.batch.item.support.SynchronizedItemStreamReader;
+import org.springframework.batch.item.support.builder.SynchronizedItemStreamReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
-import javax.annotation.PostConstruct;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableBatchProcessing
 @RequiredArgsConstructor
 @ComponentScan(basePackageClasses = {StoreItemApiService.class})
 public class BatchConfig {
-    private static final int CHUNK_SIZE = 10;
+    private static final String STORE_ITEM_JOB = "StoreItem";
+
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final StoreItemApiService storeItemApiService;
 
-    @Bean
-    @JobScope
-    @PostConstruct
-    public ItemReader<StoreItem> reader() {
-        return new StoreItemReader(storeItemApiService, null);
+    @Value("${spring.batch.config.chunkSize}")
+    private int chunkSize;
+
+    @Value("${spring.batch.config.poolSize}")
+    private int poolSize;
+
+    @Bean(name = STORE_ITEM_JOB + "Reader")
+    @StepScope
+//    @PostConstruct
+    public SynchronizedItemStreamReader<StoreItem> reader(@Value("#{jobParameters[siGunCode]}") String siGunCode) {
+        return new SynchronizedItemStreamReaderBuilder<StoreItem>()
+                .delegate(new StoreItemReader(storeItemApiService, siGunCode))
+                .build();
     }
 
-    @Bean
+    @Bean(name = STORE_ITEM_JOB + "Writer")
     public MongoItemWriter<Store> writer(MongoTemplate mongoTemplate) {
         return new StoreItemWriter(mongoTemplate);
     }
@@ -51,20 +60,35 @@ public class BatchConfig {
         return new StoreItemProcessor();
     }
 
-    @Bean
+    @Bean(name = STORE_ITEM_JOB + "TaskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(poolSize);
+        executor.setMaxPoolSize(poolSize);
+        executor.setThreadNamePrefix("multi-thread-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = STORE_ITEM_JOB + "Step")
+    @JobScope
     public Step updateStoreItemsStep(ItemReader<StoreItem> reader, ItemWriter<Store> writer) {
-        return stepBuilderFactory.get("updateStoreItemsStep")
-                .<StoreItem, Store>chunk(CHUNK_SIZE)
-                .reader(reader)
+        return stepBuilderFactory.get(STORE_ITEM_JOB + "Step")
+                .<StoreItem, Store>chunk(chunkSize)
+                .reader(reader(null))
                 .processor(processor())
                 .writer(writer)
+                .taskExecutor(taskExecutor())
+                .throttleLimit(poolSize)
                 .build();
     }
 
-    @Bean
-    public Job updateStoreItemsJob(Step collectStoreItemsStep) {
-        return jobBuilderFactory.get("updateStoreItemsJob")
-                .start(collectStoreItemsStep)
+    @Bean(name = STORE_ITEM_JOB)
+    public Job updateStoreItemsJob(Step updateStoreItemsStep) {
+        return jobBuilderFactory.get(STORE_ITEM_JOB)
+                .start(updateStoreItemsStep)
+                .preventRestart()
                 .build();
     }
 }
--- src/main/java/org/localdevelopers/payfinderbatch/reader/StoreItemReader.java
+++ src/main/java/org/localdevelopers/payfinderbatch/reader/StoreItemReader.java
@@ -3,24 +3,39 @@ package org.localdevelopers.payfinderbatch.reader;
 import lombok.RequiredArgsConstructor;
 import org.localdevelopers.payfinderbatch.api.StoreItemApiService;
 import org.localdevelopers.payfinderbatch.model.StoreItem;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-public class StoreItemReader implements ItemReader<StoreItem> {
+public class StoreItemReader implements ItemStreamReader<StoreItem> {
+
     private final StoreItemApiService storeItemApiService;
+    private final String siGunCode;
     private List<StoreItem> items = null;
     private int index = 0;
-
-    @Value("#{jobParameters[siGunCode]}")
-    private final String siGunCode;
 
     @Override
     public StoreItem read() {
         if (items == null)
             items = storeItemApiService.fetchBySiGunCode(siGunCode);
         return index < items.size() ? items.get(index++) : null;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+
     }
 }
